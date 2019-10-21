aed6a4543f761f5fb0a622dc771e588f48b3c7fe
AmadouDiallo4 <diallobis2011@gmail.com>
2019-10-21 10:09:27 +0000

    correction

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-"""ecrire ici un projet""" 
+""" test"""
 
 from flask import Flask
 
@@ -7,7 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """ Affiche Hello"""
+
     return "Hello Gens de Fitec-Faurecia!"
 
 if __name__ == "__main__":
+    """ function main"""
     app.run(host="0.0.0.0", port=5000)
